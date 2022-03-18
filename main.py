7301d95bc3f6080bcee89434412021f7280495fc
hastak-9 <101249628+hastak-9@users.noreply.github.com>
2022-03-18 21:46:37 +0100

    added monsters

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tcod
+import copy
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from procgen import generate_dungeon
 from input_handlers import EventHandler
 
@@ -17,19 +18,26 @@ def main() -> None:
     room_min_size = 6
     max_rooms = 10
 
+    max_monsters_per_room = 2
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width/2), int(screen_height/2), "@", (0, 255, 0))
-    npc = Entity(int(screen_width/2)-5, int(screen_height/2), "@", (255, 255, 0))
-    entities = {npc, player}
+    player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    game_map = generate_dungeon(
+        max_rooms,
+        room_min_size,
+        room_max_size,
+        map_width,
+        map_height,
+        max_monsters_per_room,
+        player)
 
-    engine = Engine(entities, event_handler, game_map, player)
+    engine = Engine(event_handler, game_map, player)
 
     with tcod.context.new_terminal(
         screen_width,
