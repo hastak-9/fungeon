9d6ef6fd5cc33e99eeb97be22f990cc3c51a1724
hastak-9 <101249628+hastak-9@users.noreply.github.com>
2022-03-18 11:52:05 +0100

    added light tiles

--- tile_types.py
+++ tile_types.py
@@ -6,8 +6,8 @@ import numpy as np # type: ignore
 graphic_dt = np.dtype(
     [
         ("ch", np.int32), #UNicode codepoint
-        ("fg", "3B"), # 3 unsigned bytes for RGB colors
-        ("bg", "3B"),
+        ("fg", "3B"), # 3 unsigned bytes for RGB colors fg-foreground
+        ("bg", "3B"), # bg - background color
     ]
 )
 # Tile struct used for statically defined tile data
@@ -16,6 +16,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool), #True if this tile van be walked over
         ("transparent", np.bool), #True if this thile doesnt block FOV
         ("dark", graphic_dt), #Graphics for when this tile is not in FOV
+        ("light", graphic_dt), #Graphics for when the tile is in FOV
     ]
 )
 
@@ -25,14 +26,21 @@ def new_tile(
         walkable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
     """Helper function for defining individual tile types"""
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
 
 floor = new_tile(
-    walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    walkable=True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50))
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    walkable=False,
+    transparent=False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
